24e658ba3366e3e76767fac8aa534df8ea25c74e
Debjani Banerjee <67143143+debjani-banerjee@users.noreply.github.com>
2023-06-15 19:31:43 +0530

    Changed

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,63 +1,179 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Cart.module.css";
 import Image from "next/image";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { reset } from "../redux/cartSlice";
+import OrderDetail from "../components/OrderDetail";
 
 export default function Cart() {
+  const cart = useSelector((state) => state.cart);
+  const [open, setOpen] = useState(false);
+  const [cash, setCash] = useState(false);
+  const amount = cart.total;
+  const currency = "USD";
+  const style = { layout: "vertical" };
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const createOrder = async (data) => {
+    try {
+      const res = await axios.post("http://localhost:3000/api/orders",data);
+      if (res.status === 201) {
+        dispatch(reset());
+        router.push(`/orders/${res.data._id}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+    useEffect(() => {
+      dispatch({
+        type: "resetOptions",
+        value: {
+          ...options,
+          currency: currency,
+        },
+      });
+    }, [currency, showSpinner]);
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={async (data, actions) => {
+            const orderId = await actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              });
+            return orderId;
+          }}
+          onApprove={async function (data, actions) {
+            const details = await actions.order.capture();
+            const shipping = details.purchase_units[0].shipping;
+            createOrder({
+              customer: shipping.name.full_name,
+              address: shipping.address_line_1,
+              total: cart.total,
+              method: 1,
+            });
+          }}
+        />
+      </>
+    );
+  };
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <table className={styles.table}>
           <tbody>
-          <tr className={styles.trTitle}>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Extras</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-          </tr>
-          <tr className={styles.tr}>
-            <td>
-              <div className={styles.imgContainer}>
-              <Image src="/img/pizza2.png" alt="" layout="fill" objectFit="cover" />
-              </div>   
-            </td>
-            <td>
-              <span className={styles.name}>PEPPERONI PARTY</span>
-            </td>
-            <td>
-              <span className={styles.extras}>
-                Double Ingredient, Spicy Sauce
-              </span>
-            </td>
-            <td>
-              <span className={styles.price}>₹119</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>₹238</span>
-            </td>
-          </tr>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </tbody>
+          <tbody>
+            {cart.products.map((product) => (
+              <tr className={styles.tr} key={product._id}>
+                <td>
+                  <div className={styles.imgContainer}>
+                    <Image
+                      src={product.img}
+                      alt=""
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </td>
+                <td>
+                  <span className={styles.name}>{product.title}</span>
+                </td>
+                <td>
+                  <span className={styles.extras}>
+                    {product.extras.map((extra) => (
+                      <span key={extra._id}>{extra.text}, </span>
+                    ))}
+                  </span>
+                </td>
+                <td>
+                  <span className={styles.price}>₹{product.price}</span>
+                </td>
+                <td>
+                  <span className={styles.quantity}>{product.quantity}</span>
+                </td>
+                <td>
+                  <span className={styles.total}>
+                    ₹{product.price * product.quantity}
+                  </span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
-            <h2 className={styles.title}>CART TOTAL</h2>
-            <div className={styles.totalText}>
-                <b className={styles.totalTextTitle}>Subtotal:</b>₹238.00
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>₹{cart.total}
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>₹0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>₹{cart.total}
+          </div>
+          {open ? (
+            <div className={styles.paymentMethods}>
+              <button className={styles.payButton} onClick={()=>setCash(true)}>CASH ON DELIVERY</button>
+              <PayPalScriptProvider
+                options={{
+                  "client-id":
+                    "AdsH5ys5Zz2R1t4Aa81IIYreHUuVffFh3cd2a8b5RihBiSWf0OaM25tcM8V-OgAnh43Q8CGXkK4LWa_Y",
+                  components: "buttons",
+                  currency: "USD",
+                  "disable-funding": "credit,card,p24",
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
             </div>
-            <div className={styles.totalText}>
-                <b className={styles.totalTextTitle}>Discount:</b>₹50.00
-            </div>
-            <div className={styles.totalText}>
-                <b className={styles.totalTextTitle}>Total:</b>₹188.00
-            </div>
-            <button className={styles.button}>CHECKOUT NOW!</button>
+          ) : (
+            <button onClick={() => setOpen(true)} className={styles.button}>
+              CHECKOUT NOW!
+            </button>
+          )}
         </div>
       </div>
+      {cash && <OrderDetail total={cart.total} createOrder={createOrder} />}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Featured from '../components/Featured'
-import ProductList from '../components/ProductList'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Add from "../components/Add";
+import AddButton from "../components/AddButton";
+import Featured from "../components/Featured";
+import ProductList from "../components/ProductList";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({productList}) {
+  const [close, setClose] = useState(true);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,8 +17,27 @@ export default function Home() {
         <meta name="description" content="Door-Step Food Delivery" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Featured/>
-      <ProductList/>
+      <Featured />
+      {<AddButton setClose={setClose} />}
+      <ProductList productList={productList} />
+      {!close && <Add setClose={setClose} />}
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async (ctx) => {
+
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      productList: res.data,
+    },
+  };
+};
